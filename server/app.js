402d1873f887b48a2ab40cb5a66d8aa9e4b551a5
lambui <lbui9@ucmerced.edu>
2018-05-22 20:55:52 -0700

    fortify with HTTPS

--- server/app.js
+++ server/app.js
@@ -1,3 +1,8 @@
+const fs = require('fs'); //package to read file on file system (fs)
+const https = require('https');
+const crypto = require('crypto');
+
+//load express
 const express = require('express');
 const app = express();
 
@@ -10,7 +15,11 @@ const port = properties.port;
 const userdb = require("./users.json");
 
 //middlewares
-//bodyParser
+//Helmet: force HTTP request into HTTPS request (HSTS)
+const helmet = require('helmet');
+app.use(helmet());
+
+//bodyParser: read json from request body
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
@@ -29,7 +38,16 @@ app.post("/users", (req, res) => {
 });
 
 //start server
-app.listen(port, hostname, (req, res)=>{
-  console.log(`Server running at http://${hostname}:${port}/`);
+//load private key and cert
+//const symmetric_key = crypto.createDiffieHellman(2048).generateKeys(); //create symetric key for key exchange encryption on server starts
+const https_options = 
+{
+  key: fs.readFileSync('cert/key.pem'),
+  cert: fs.readFileSync('cert/cert.pem'),
+  //dhparam: symmetric_key, 
+  dhparam: fs.readFileSync('cert/symmetric_key.pem')
+};
+https.createServer(https_options, app).listen(port, hostname, (req, res) => {
+  console.log(`Server starts on https://${hostname}:${port}`);
 });
 
