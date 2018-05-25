2fb76c23625dc63d55ea7f9b12f3ee6ca4559fe3
lambui <lbui9@ucmerced.edu>
2018-05-24 22:36:27 -0700

    create login POST action

--- server/app.js
+++ server/app.js
@@ -12,7 +12,8 @@ const hostname = properties.ip;
 const port = properties.port;
 
 //load mock database
-const userdb = require("./users.json");
+var userdb = require("./users.json");
+var tokendb = require("./tokens.json");
 
 //middlewares
 //Helmet: force HTTP request into HTTPS request (HSTS)
@@ -35,6 +36,35 @@ app.get("/users", (req, res) => {
 app.post("/users", (req, res) => {
   console.log(req.body);
   res.send(req.body);
+});
+
+app.post("/login", (req, res) => {
+  let body = req.body;
+  for (let i in userdb.users) //for loop searching for user in userdb should be a query if using a real database
+  {
+    let user = userdb.users[i];
+    if(user.username === body.username && user.password === body.password)
+    {
+      //generate random hash as session token
+      let current_date = (new Date()).valueOf().toString();
+      let random = Math.random().toString();
+      let token = crypto.createHash('sha1').update(current_date + random).digest('hex');
+
+      //store session_token in tokens.json
+      let login_session = {};
+      login_session.username = body.username;
+      login_session.token = token;
+      tokendb.tokens.push(login_session);
+      fs.writeFile("./tokens.json", JSON.stringify(tokendb), (err)=>{if(err!=null){console.log(err)}});
+      tokendb = require("./tokens.json");
+
+      //send success response
+      res.status(200).send("Successfully login! Session token: " + token);
+      return;
+    }
+  }
+  //send fail response
+  res.status(401).send("Wrong username or password!");
 });
 
 //start server
